c3ca81801f4de366dcad99e09dd228abe70c6ace
Kushang Tanawala <kushangtanawala24@gmail.com>
2024-08-06 09:51:03 +0530

    Create Recipe With CRUD And Roll Base Login

--- Recipe/config/database.js
+++ Recipe/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const db = async () => {
     try {
-        await mongoose.connect("Enter Your DataBase Link");
+        await mongoose.connect("Enter The DataBase Link");
         console.log("Database connected...");
     } catch (error) {
         console.log(error);
