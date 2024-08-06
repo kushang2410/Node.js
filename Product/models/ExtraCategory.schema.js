d74cf796554f8cd5a79a7c6d35a5d01a75c5acc9
Kushang Tanawala <kushangtanawala24@gmail.com>
2024-08-06 09:54:49 +0530

    Delete Product directory

--- Product/models/ExtraCategory.schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ExtraCategorySchema = new mongoose.Schema({
-    name: String
-});
-
-const ExtraCategoryData = mongoose.model("ExtraCategory-Table", ExtraCategorySchema);
-
-module.exports = ExtraCategoryData;
--- Product/routers/ExtraCategory.router.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { ExtraCategoryCreate , GetAllExtraCategories } = require("../controllers/ExtraCategory.controller");
-
-const ExtraCategoryRouter = Router();
-
-ExtraCategoryRouter.post('/create', ExtraCategoryCreate);
-ExtraCategoryRouter.get('/', GetAllExtraCategories);
-
-module.exports = ExtraCategoryRouter;
