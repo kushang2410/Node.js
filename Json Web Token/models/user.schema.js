78696ebd911d21b31f004f0f97f8a227537fecb2
Kushang Tanawala <kushangtanawala24@gmail.com>
2024-08-18 08:31:03 +0530

    JWT Token Create And CRUD Operation in Postman

---
+++ Json Web Token/models/user.schema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const AuthSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const DataAuth = mongoose.model("userData", AuthSchema);
+
+module.exports = DataAuth;
