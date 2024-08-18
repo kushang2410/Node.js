46779616a79f905d576223350b4ebc5a53d291a2
Kushang Tanawala <kushangtanawala24@gmail.com>
2024-08-18 08:29:58 +0530

    JWT Token Create And CRUD Operation in Postman

---
+++ Json Web Token/config/DataBase.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const db = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://kushangtanawala:1234@cluster0.i10uonz.mongodb.net/JWT");
+        console.log("Database connected...");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = db;
