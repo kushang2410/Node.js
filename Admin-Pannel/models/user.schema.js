51677bc220eee7776e88d150dbee61247178d511
Kushang Tanawala <kushangtanawala24@gmail.com>
2024-08-29 01:03:44 +0530

    Admin Panel With Passport.js Authentication

---
+++ Admin-Pannel/models/user.schema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const AuthSchema = new mongoose.Schema({
+    name:String,
+    email: String,
+    password: String,
+    profilePhoto:String,
+    phone: Number
+});
+
+const DataAuth = mongoose.model("userData", AuthSchema);
+
+module.exports = DataAuth;
