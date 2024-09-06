050ea11c35859eadfa59b6f2692161bfb956becd
Kushang Tanawala <kushangtanawala24@gmail.com>
2024-09-06 17:56:40 +0530

    Create ,Update ,Delete Blogs With Passport.js

--- Blogs/config/DataBase.js
+++ Blogs/config/DataBase.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const db = async () => {
-    await mongoose.connect("Enter Your DataBase Link")
+    await mongoose.connect("Enter DataBase Link")
     console.log("database connected");
 }
 
